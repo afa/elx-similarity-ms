47ba9914dc0fc9307fe780de8fe1fdbaf661a476
Afa <afa.alone@gmail.com>
2024-09-29 04:07:10 +0300

    dashboard counts

--- lib/app/schemas/similarity/model.ex
+++ lib/app/schemas/similarity/model.ex
@@ -1,5 +1,7 @@
 defmodule Similarity.Model do
   use Ecto.Schema
+  import Ecto.Query
+
   @derive {Jason.Encoder, only: [:id, :name, :state]}
 
   schema "similarity_model" do
@@ -7,5 +9,12 @@ defmodule Similarity.Model do
     field :state, Ecto.Enum, values: [disabled: 0, enabled: 1]
     field :avg_doc_length, :float
     has_many :documents, Similarity.Document
+  end
+
+  def active do
+    q = from m in Similarity.Model,
+      where: m.state == :enabled,
+      select: m
+    App.Repo.all(q)
   end
 end
--- lib/app_web/controllers/api/v1/dashboard_controller.ex
+++ lib/app_web/controllers/api/v1/dashboard_controller.ex
@@ -2,10 +2,31 @@ defmodule AppWeb.Api.V1.DashboardController do
   use AppWeb, :controller
 
   def index(con, _params) do
-    models = list()
     con
-    |> json(%{models: models})
+    |> json(payload())
   end
 
-  defp list, do: App.Repo.all(Similarity.Model)
+  defp payload do
+    %{
+      models: models(),
+      documents_total: documents_total(),
+      documents_ranked: documents_ranked(),
+      documents_tokenized: documents_tokenized(),
+      tokens_total: tokens_total()
+    }
+  end
+
+  defp models, do: Similarity.Model.active
+  defp documents_total, do: Similarity.Document.total
+  defp tokens_total, do: Similarity.Token.total
+
+  defp documents_ranked do
+    Similarity.Document.ranked
+    |> Similarity.Document.total
+  end
+
+  defp documents_tokenized do
+    Similarity.Document.tokenized
+    |> Similarity.Document.total
+  end
 end
