637ff4f75999500053156bf779c38e7a14fb42db
afa <afa.alone@gmail.com>
2024-09-29 04:12:48 +0300

    dashboard counts (#8)

--- lib/app/schemas/similarity/document.ex
+++ lib/app/schemas/similarity/document.ex
@@ -1,5 +1,6 @@
 defmodule Similarity.Document do
   use Ecto.Schema
+  import Ecto.Query
 
   schema "similarity_document" do
     field :name, :string
@@ -9,5 +10,14 @@ defmodule Similarity.Document do
     field :tokenized_at, :integer
     field :ranked_at, :integer
     belongs_to :model, Similarity.Model
+  end
+
+  def tokenized, do: from(d in Similarity.Document, where: d.state == :tokenized)
+  def ranked, do: from(d in Similarity.Document, where: d.state == :ranked)
+
+  def total(queryable \\ __MODULE__) do
+    queryable
+    |> select([s], count())
+    |> App.Repo.one
   end
 end
--- lib/app/schemas/similarity/token.ex
+++ lib/app/schemas/similarity/token.ex
@@ -1,8 +1,17 @@
 defmodule Similarity.Token do
   use Ecto.Schema
+  import Ecto.Query
 
   schema "similarity_token" do
     field :value, :string
     has_many :counts, Similarity.TokenCount
+  end
+
+  def total do
+    from(
+      m in Similarity.Token,
+      select: count()
+    )
+    |> App.Repo.one
   end
 end
